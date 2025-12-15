cdd3ce9332fc4230077e33fb1f494b0063035198
MANIKANDAN A <iammanikandan.engineer@gmail.com>
2025-12-15 10:58:37 +0530

    u

--- backend/fix_role.sql
+++
@@ -1,2 +0,0 @@
-UPDATE users SET role = 'ROLE_RESTAURANT_OWNER' WHERE email = 'iammanikandan.engineer@gmail.com';
-SELECT id, email, role FROM users WHERE email = 'iammanikandan.engineer@gmail.com';
